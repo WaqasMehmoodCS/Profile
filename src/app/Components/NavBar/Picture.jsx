e53c6e0571c01518451a7009550c5f5e1f7da33d
Waqas Mehmood <123810942+WaqasMehmoodCS@users.noreply.github.com>
2024-02-15 14:50:50 +0400

    working on navigation

---
+++ src/app/Components/NavBar/Picture.jsx
@@ -0,0 +1,16 @@
+
+const Picture = () => {
+    return (
+        <>
+            <div>Picture</div>
+            <div>Social Media Icons</div>
+            <div>Home</div>
+            <div>About</div>
+            <div>Services</div>
+            <div>Portfolio</div>
+            <div>REsume</div>
+        </>
+    )
+}
+
+export default Picture
