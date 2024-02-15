45c5e84ef63c1d4f45f4a769dadb5f5e0332051d
Waqas Mehmood <123810942+WaqasMehmoodCS@users.noreply.github.com>
2024-02-15 16:52:26 +0400

    picture added

--- src/app/Components/NavBar/Navigation.jsx
+++ src/app/Components/NavBar/Navigation.jsx
@@ -8,8 +8,9 @@ const Navigation = () => {
     const { open } = useContext(MenuContext)
     return (
         <div className={`ease-in-out ${open ? 'translate-x-0' : '-translate-x-[1500px]'} bg-secondary/90 max-w-80 text-primary p-4 rounded-r-lg duration-500 sm:translate-x-0 h-screen`}>
-
-            <Picture />
+            <div>
+                <Picture />
+            </div>
 
 
         </div>
--- src/app/Components/NavBar/Picture.jsx
+++ src/app/Components/NavBar/Picture.jsx
@@ -1,15 +1,12 @@
+import Image from "next/image"
 
 const Picture = () => {
     return (
-        <>
-            <div>Picture</div>
-            <div>Social Media Icons</div>
-            <div>Home</div>
-            <div>About</div>
-            <div>Services</div>
-            <div>Portfolio</div>
-            <div>REsume</div>
-        </>
+        <div className="flex justify-center pt-4">
+            <div className="w-36 h-36 absolute rounded-full overflow-hidden object-cover ring-8">
+                <Image src='/pic.png' alt="Picture, Developer, React Developer picture" fill priority sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
+            </div>
+        </div>
     )
 }
 
