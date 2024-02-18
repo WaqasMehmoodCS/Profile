b25e0f6c967b0801baaf635db2964f3a2a67602f
Waqas Mehmood <123810942+WaqasMehmoodCS@users.noreply.github.com>
2024-02-18 12:23:27 +0400

    skills readjusted

--- src/Components/MainContent/Homepage/Typo.jsx
+++ src/Components/MainContent/Homepage/Typo.jsx
@@ -2,7 +2,7 @@
 import { Typewriter } from 'react-simple-typewriter'
 
 const Typo = () => {
-    const words = ['Frontend Developer', 'React JS Developer', 'Full Stack Developer']
+    const words = ['Frontend Developer', 'React JS Developer','Next JS Developer', 'Backend Developer', 'Full Stack Developer', 'MERN Stack Developer']
     return (
         <div className='text-other underline font-manrope tracking-widest'><Typewriter words={words} loop cursor cursorBlinking cursorColor='white' typeSpeed={100} deleteSpeed={70} /></div>
     )
--- src/Components/NavBar/Picture.jsx
+++ src/Components/NavBar/Picture.jsx
@@ -1,10 +1,13 @@
+import { MenuContext } from "@/Context/MenuContextWrapper"
 import Image from "next/image"
 import Link from "next/link"
+import { useContext } from "react"
 
 const Picture = () => {
+    const { open, setOpen } = useContext(MenuContext)
     return (
         <div className="pt-4">
-            <Link href={'/'} className="flex justify-center" >
+            <Link href={'/'} onClick={() => { setOpen(!open) }} className="flex justify-center" >
                 <div className="w-36 h-36 absolute rounded-full overflow-hidden object-cover ring-8">
                     <Image src='/pic.png' alt="Picture, Developer, React Developer picture" fill priority sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
                 </div>
