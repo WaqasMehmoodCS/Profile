fbb125ef762fa871b2593c0d2612a5db788d7f1a
Waqas Mehmood <123810942+WaqasMehmoodCS@users.noreply.github.com>
2024-02-19 11:29:48 +0400

    resume completed

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -1,50 +1,20 @@
-
+import Link from 'next/link';
 
 const ResumePage = () => {
     return (
-        <>
-            <div className='text-primary h-full flex flex-col overflow-y-scroll mt-0 p-4'>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
-                <div>ResumePage</div>
+        <div className="p-4 sm:mt-8 lg:mt-10 h-full flex font-manrope overflow-auto">
+            <div className="w-full h-full flex justify-center items-center">
+                <iframe
+                    src="/documents/cs_resume.pdf"
+                    title="Resume"
+                    className="w-full h-full"
+                ></iframe>
             </div>
-        </>
-    )
-}
+            <div className="text-center mt-4">
+                <Link href="/documents/cs_resume.pdf" download />
+            </div>
+        </div>
+    );
+};
 
-export default ResumePage
+export default ResumePage;
