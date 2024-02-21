db8f251658d5413383423282713375cc6ea235f6
Waqas Mehmood <123810942+WaqasMehmoodCS@users.noreply.github.com>
2024-02-21 22:06:19 +0400

    adjusted env variable

--- src/utils/ConnectDB.js
+++ src/utils/ConnectDB.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
 const connectDB = async () => {
+  dotenv.config();
   try {
-    const db = await mongoose.connect(process.env.MONGO);
+    const db = await mongoose.connect(process.env.NEXT_PUBLIC_MONGO);
 
-    console.log(`DB is connected at port: ${db.connection.port}`);
-    console.log(db.connection.name);
+    // console.log(`DB is connected at port: ${db.connection.port}`);
+    // console.log(db.connection.name);
   } catch (error) {
     console.error("Error connecting to the database:", error);
   }
