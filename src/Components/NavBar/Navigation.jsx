ec614b01b541c1933b15a4d18464315f3d0fb51e
Waqas Mehmood <123810942+WaqasMehmoodCS@users.noreply.github.com>
2024-04-29 14:54:31 +0400

    mobile settings readjusted

--- src/Components/NavBar/Navigation.jsx
+++ src/Components/NavBar/Navigation.jsx
@@ -9,7 +9,7 @@ import Name from "./Name"
 const Navigation = () => {
     const { open } = useContext(MenuContext)
     return (
-        <div className={`bg-black/90 h-screen absolute top-0 left-0 w-2/3 md:w-2/6 ease-in-out duration-1000 text-primary ${open ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 flex flex-col z-50 justify-around`}>
+        <div className={`bg-black/90 h-screen absolute top-0 left-0 w-2/3 md:w-2/6 ease-in-out duration-1000 text-primary ${open ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 flex flex-col z-50 gap-5`}>
             <Picture />
             <div className="flex flex-col gap-4">
                 <Name />
