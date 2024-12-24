023f38cd07d384101264a89373627b39ebd514dc
waqas mehmood <waqasmehmoodcs@gmail.com>
2024-12-24 16:06:26 +0500

    portfolio updated

--- src/app/portfolio/LinkComponent.jsx
+++ src/app/portfolio/LinkComponent.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const LinkComponent = ({ link, name }) => {
     return (
         <Link href={link}>
-            <div className="w-full p-4 flex justify-center items-center bg-other text-primary ring-1 rounded-lg font-bold sm:text-xl tracking-wider capitalize">
+            <div className="text-sm w-full p-3 flex justify-center items-center bg-other text-primary ring-1 rounded-lg font-bold sm:text-xl tracking-wider capitalize">
                 <div>{name}</div>
             </div>
         </Link>
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,28 +1,49 @@
-import ImageArray from "./ImageArray"
-import LinkComponent from "./LinkComponent"
+import ImageArray from "./ImageArray";
+import LinkComponent from "./LinkComponent";
 
 export const metadata = {
-    title: "Portfolio",
-    description: "Familiar Technologies, about, qualification, skills, experience, message, contact, service, projects",
+  title: "Portfolio",
+  description:
+    "Familiar Technologies, about, qualification, skills, experience, message, contact, service, projects",
 };
 
 const PortfolioPage = () => {
-    const images = [
-        { id: 1, path: '/1.webp' },
-        { id: 2, path: '/2.webp' },
-        { id: 3, path: '/3.webp' },
-    ];
-    const name = 'Portfolio Application'
-    return (
-        <div className="p-4 mt-2 sm:mt-8 lg:mt-10 font-manrope w-2/3 m-auto flex flex-col gap-3">
-            <div className="bg-slate-500/70 rounded-lg text-primary w-full animate-scale-up-center">
-                <div className="flex flex-col gap-6 p-4">
-                    <ImageArray images={images} />
-                    <LinkComponent link={'https://profile-waqas.vercel.app/'} name={name} />
-                </div>
-            </div>
+  const images = [
+    { id: 1, path: "/1.webp" },
+    { id: 2, path: "/2.webp" },
+    { id: 3, path: "/3.webp" },
+  ];
+  const weatherImages = [
+    { id: 1, path: "/4.webp" },
+    { id: 2, path: "/5.webp" },
+    { id: 3, path: "/6.webp" },
+    { id: 4, path: "/7.webp" },
+    { id: 5, path: "/8.webp" },
+  ];
+  const name = "Portfolio Application";
+  const weatherName = "Weather Forecast Application";
+  return (
+    <div className="h-full px-2 pb-14 flex flex-col gap-3 md:gap-8 pt-3 md:pb-5 font-manrope md:pt-5 md:px-16 overflow-auto scroll-hide scroll-btn">
+      <div className="bg-slate-500/70 rounded-lg text-primary w-full animate-scale-up-center">
+        <div className="flex flex-col gap-6 p-4">
+          <ImageArray images={weatherImages} />
+          <LinkComponent
+            link={"https://seasondemo.netlify.app/"}
+            name={weatherName}
+          />
         </div>
-    )
-}
+      </div>
+      <div className="bg-slate-500/70 rounded-lg text-primary w-full animate-scale-up-center">
+        <div className="flex flex-col gap-6 p-4">
+          <ImageArray images={images} />
+          <LinkComponent
+            link={"https://profile-waqas.vercel.app/"}
+            name={name}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default PortfolioPage
+export default PortfolioPage;
